46f650799b1a2caceb630a556ae20476e86a0268
Florian HENRY <florian.henry@open-concept.pro>
2022-10-14 10:29:30 +0200

    clan code and credentials

--- core/modules/modPriseo.class.php
+++ core/modules/modPriseo.class.php
@@ -45,7 +45,7 @@ class modPriseo extends DolibarrModules
 
 		// Id for module (must be unique).
 		// Use here a free id (See in Home -> System information -> Dolibarr for list of used modules id).
-		$this->numero = 43635; // TODO Go on page https://wiki.dolibarr.org/index.php/List_of_modules_id to reserve an id number for your module
+		$this->numero = 436360;
 
 		// Key text used to identify module (for permissions, menus, etc...)
 		$this->rights_class = 'priseo';
